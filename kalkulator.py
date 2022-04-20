f56502b967112a611e678f15402dab2fca6b363d
Dorota Pietruszka <do.pietruszka@gmail.com>
2022-04-20 10:34:28 +0200

    logging.info correction

--- kalkulator.py
+++ kalkulator.py
@@ -9,26 +9,24 @@ def pomnoz(x, y):
   return x * y
 def podziel(x, y):
   return x / y
-choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-num1 = float(input("Podaj składnik 1: "))
-num2 = float(input("Podaj składnik 2: "))
-if choice == '1':
-  logging.info("dodaję", num1, "i", num2)
-  print("wynik to", dodaj(num1,num2))
-elif choice == '2':
-  logging.info("od", num1, "odejmuję", num2)
-  print("wynik to", odejmij(num1,num2))
-elif choice == '3':
-  logging.info("mnożę", num1, "i", num2)
-  print("wynik to", pomnoz(num1,num2))
-elif choice == '4':
-  logging.info("dzielę", num1, "przez", num2)
-  print("wynik to", podziel(num1,num2))
-else:
-  logging.info("niepoprawna wartość")
+
 
 if __name__ == "__main__":
-    logging.debug("The program was called with this parameters %s and %s" % (sys.argv[1], sys.argv[2]))
-    x = sys.argv[1]
-    y = sys.argv[2]
+    choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    num1 = float(input("Podaj składnik 1: "))
+    num2 = float(input("Podaj składnik 2: "))
+    if choice == '1':
+      logging.info("Dodaje: %f %f", num1, num2)
+      print("wynik to", dodaj(num1,num2))
+    elif choice == '2':
+      logging.info("Odejmuję: %f %f", num1, num2)
+      print("wynik to", odejmij(num1,num2))
+    elif choice == '3':
+      logging.info("Mnożę: %f %f", num1, num2)
+      print("wynik to", pomnoz(num1,num2))
+    elif choice == '4':
+      logging.info("Dzielę: %f %f", num1, num2)
+      print("wynik to", podziel(num1,num2))
+    else:
+      logging.info("niepoprawna wartość")
 
